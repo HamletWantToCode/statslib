8b6e6d8186c397c807ed78d7439879c9d6387f63
hongbinren <hongbinrenscu@outlook.com>
2018-12-17 22:01:10 +0800

    local update

--- main/cross_validation.py
+++ main/cross_validation.py
@@ -3,20 +3,22 @@
 import numpy as np 
 
 class Cross_validation(object):
-    def __init__(self, data_gen, solver, measure):
+    def __init__(self, data_gen, solver, kernel_gd):
         self.id = data_gen
         self.solver = solver
-        self.measure = measure
+        self.kernel_gd = kernel_gd
 
-    def run(self, X, y):
+    def run(self, X, y, dy):
         performance = 0
         n = 0
         for (test_id, valid_id) in self.id:
-            trainX, trainy = X[valid_id], y[valid_id]
-            testX, testy = X[test_id], y[test_id]
-            self.solver.fit(trainX, trainy)
-            predicty = self.solver.predict(testX)
-            score = self.measure(np.squeeze(predicty), np.squeeze(testy))
+            train_X, train_y = X[valid_id], y[valid_id]
+            test_X, test_y, test_dy = X[test_id], y[test_id], dy[test_id]
+            self.solver.fit(train_X, train_y)
+            predict_y = self.solver.predict(test_X)
+            K_gd = self.kernel_gd(test_X, train_X)
+            predict_dy = 501*(K_gd @ self.solver.coef_).reshape(test_dy.shape)
+            score = np.mean((predict_y - test_y)**2) + np.mean(np.mean((predict_dy - test_dy)**2, axis=1))
             performance += score**2
             n += 1
         return np.sqrt(performance*1.0/n)
